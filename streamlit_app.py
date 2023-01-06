84710b9236dc592fb3dcf733a0ac4aefb0cf9f64
jayvannelli <jaymvannelli@gmail.com>
2023-01-06 17:10:21 -0500

    Clean up file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,9 @@
 import streamlit as st
-import pandas as pd
-
-
-def get_data() -> pd.DataFrame:
-    """Return hotel reservation CSV data as pandas DataFrame."""
-    _df = pd.read_csv("data/Hotel_Reservations.csv")
-    return _df
+from src.data import df
 
 
 def main():
     st.title("Hotel Reservations Dashboard")
-
-    df = get_data()
 
     st.write(df)
 
